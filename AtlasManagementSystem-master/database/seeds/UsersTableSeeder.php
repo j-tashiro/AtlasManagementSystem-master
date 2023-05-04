55513e7c1964046a64ffb129105270489add2461
juntaro1126 <blacktoblack345@yahoo.co.jp>
2023-05-05 08:23:09 +0900

    2023.05.05 対応 サイドバーの項目の一部を講師アカウントのみに表示させる 2023.05.03 完了 初期データの登録

--- AtlasManagementSystem-master/database/seeds/UsersTableSeeder.php
+++ AtlasManagementSystem-master/database/seeds/UsersTableSeeder.php
@@ -23,5 +23,38 @@ class UsersTableSeeder extends Seeder
             'role' => '4',
             'password' => bcrypt('password'),
         ]);
+        DB::table('users')->insert([
+            'over_name' => '国語',
+            'under_name' => '先生',
+            'over_name_kana' => 'コクゴ',
+            'under_name_kana' => 'センセイ',
+            'mail_address' => 'kokugo.sensei@gmail.com',
+            'sex' => '1',
+            'birth_day' => '1993-11-26',
+            'role' => '1',
+            'password' => bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'over_name' => '数学',
+            'under_name' => '先生',
+            'over_name_kana' => 'スウガク',
+            'under_name_kana' => 'センセイ',
+            'mail_address' => 'sugaku.sensei@gmail.com',
+            'sex' => '1',
+            'birth_day' => '1993-11-26',
+            'role' => '2',
+            'password' => bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'over_name' => '英語',
+            'under_name' => '先生',
+            'over_name_kana' => 'エイゴ',
+            'under_name_kana' => 'センセイ',
+            'mail_address' => 'eigo.sensei@gmail.com',
+            'sex' => '1',
+            'birth_day' => '1993-11-26',
+            'role' => '3',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
