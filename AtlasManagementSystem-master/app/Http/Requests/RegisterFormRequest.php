9ee7b363cca2d675864d00a059004325a1348ea9
juntaro1126 <blacktoblack345@yahoo.co.jp>
2023-05-15 16:01:47 +0900

    2023.05.15 対応 サイドバーの項目の一部を講師アカウントのみに表示させる
    2023.05.15 完了 ユーザーの新規登録画面にバリデーションを追加
    2023.05.11 完了 usersテーブルとsubjectsテーブルのリレーションを定義
    2023.05.03 完了 初期データの登録

--- AtlasManagementSystem-master/app/Http/Requests/RegisterFormRequest.php
+++ AtlasManagementSystem-master/app/Http/Requests/RegisterFormRequest.php
@@ -22,22 +22,24 @@ class RegisterFormRequest extends FormRequest
      *
      * @return array
      */
-    // 2023.05.12 ユーザーの新規登録画面にバリデーションを追加
+    // 2023.05.15 ユーザーの新規登録画面にバリデーションを追加
     public function rules()
     {
         return [
             'over_name' => 'required|max:10|string',
             'under_name' => 'required|max:10|string',
-            // 2023.05.13 カタカナのバリデーション regex:/\A[ァ-ヶー]+\z/u
+            // 2023.05.13 カタカナのバリデーション regex:/\A[ァ-ヶー]+\z/u 苦労した所
             'over_name_kana' => 'required|max:30|string|regex:/\A[ァ-ヶー]+\z/u',
             'under_name_kana' => 'required|max:30|string|regex:/\A[ァ-ヶー]+\z/u',
             'mail_address' => 'required|max:100|email|unique:users,mail_address',
+            // 2023.05.15 ユーザーの新規登録画面にバリデーションを追加 苦労した所
             'sex' => 'required|in:1,2,3,',
             'old_year' => 'required|integer',
             'old_month' => 'required',
             'old_day' => 'required',
             'role' => 'required|in:1,2,3,4,',
             'password' => 'required|min:8|max:30|confirmed',
+            // 2023.05.15 ユーザーの新規登録画面にバリデーションを追加 苦労した所
             'birthDate' => 'required|after_or_equal:2000/1/1|date|date_format:Y-m-d',
         ];
     }
@@ -45,6 +47,7 @@ class RegisterFormRequest extends FormRequest
     protected function prepareForValidation()
     {
         $this->merge([
+            // 2023.05.15 ユーザーの新規登録画面にバリデーションを追加 苦労した所
             'birthDate' => $this->input('old_year') . '-' . $this->input('old_month') . '-' . $this->input('old_day')
         ]);
     }
