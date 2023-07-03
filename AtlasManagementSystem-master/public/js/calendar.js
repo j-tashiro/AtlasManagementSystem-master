a7d246e9e43ae115d774600b0756ad1fa12d899c
juntaro1126 <blacktoblack345@yahoo.co.jp>
2023-07-03 20:52:13 +0900

    2023.07.03 対応予約をキャンセルをするための確認画面を追加 2023.07.03 対応予約をキャンセルする機能の追加
    2023.06.25 完了予約している人数を表示
    2023.06.13 完了 サブカテゴリーの追加 提出&確認用
    2023.06.21 完了 予約画面の表示形式を変更
    2023.06.13 完了 選択科目での検索機能を追加
    2023.06.13 完了 検索条件欄に選択科目の表示を追加
    2023.05.30 完了 一覧画面で選択科目の表示
    2023.05.27 完了 コメントの数を表示
    2023.05.23 完了 投稿の編集/削除
    2023.05.20 完了 いいねの数を表示
    2023.05.17 完了 タイムアウトするとエラーが表示される
    2023.05.16 完了 サイドバーの項目の一部を講師アカウントのみに表示させる
    2023.05.15 完了 ユーザーの新規登録画面にバリデーションを追加→バリデーション設定も確認必須
    2023.05.11 完了 usersテーブルとsubjectsテーブルのリレーションを定義
    2023.05.03 完了 初期データの登録
    2023.05.30 完了 デバッグバーの導入

--- AtlasManagementSystem-master/public/js/calendar.js
+++ AtlasManagementSystem-master/public/js/calendar.js
@@ -8,7 +8,7 @@ $(function(){
 
 
 // 2023.06.25 予約をキャンセルをするための確認画面を追加
-//要素を取得
+//要素を取得 let=変数 const=定数
 const modal = document.querySelector('.js-modal'),
     open = document.querySelector('.js-modal-open'),
     close = document.querySelector('.js-modal-close');
