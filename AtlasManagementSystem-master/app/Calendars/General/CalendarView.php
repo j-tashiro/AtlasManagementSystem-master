056f84d4b58effae3e13c243dd213f657cac4952
juntaro1126 <blacktoblack345@yahoo.co.jp>
2023-07-08 17:46:18 +0900

    2023.07.03 対応予約をキャンセルをするための確認画面を追加 2023.07.03 対応予約をキャンセルする機能の追加
    2023.06.25 完了予約している人数を表示
    2023.06.13 完了 サブカテゴリーの追加 提出&確認用
    2023.06.21 完了 予約画面の表示形式を変更
    2023.06.13 完了 選択科目での検索機能を追加
    2023.06.13 完了 検索条件欄に選択科目の表示を追加
    2023.05.30 完了 一覧画面で選択科目の表示
    2023.05.27 完了 コメントの数を表示
    2023.05.23 完了 投稿の編集/削除
    2023.05.20 完了 いいねの数を表示
    2023.05.17 完了 タイムアウトするとエラーが表示される
    2023.05.16 完了 サイドバーの項目の一部を講師アカウントのみに表示させる
    2023.05.15 完了 ユーザーの新規登録画面にバリデーションを追加→バリデーション設定も確認必須
    2023.05.11 完了 usersテーブルとsubjectsテーブルのリレーションを定義
    2023.05.03 完了 初期データの登録
    2023.05.30 完了 デバッグバーの導入

--- AtlasManagementSystem-master/app/Calendars/General/CalendarView.php
+++ AtlasManagementSystem-master/app/Calendars/General/CalendarView.php
@@ -67,18 +67,18 @@ class CalendarView{
           if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){//過去の予約した日
             $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px">'.$reservePart.'参加</p>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
-          }else{//未来の予約した日 モーダルを開くボタン=modal-open js-modal-open
+          }else{//未来の予約した日 
             $html[] = '<button type="submit" class="modal-open js-modal-open btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'</button>';
 
 
 
-            // 2023.06.25 予約をキャンセルをするための確認画面を追加
+            // 2023.07.08 予約をキャンセルをするための確認画面を追加
             $html[] = '<div class="modal js-modal">';
             $html[] = '<div class="modal-container">';
-            $html[] = '<div class="modal-close js-modal-close">';
-            $html[] = '×</div>';
+            $html[] = '<div class="modal-close js-modal-close">×</div>';
             $html[] = '<div class="modal-content">';
             $html[] = '<p>開きました。</p>';
+            $html[] = '</div>';
             $html[] = '</div>';
             $html[] = '</div>';
 
--- AtlasManagementSystem-master/public/js/calendar.js
+++ AtlasManagementSystem-master/public/js/calendar.js
@@ -6,9 +6,9 @@ $(function(){
 
 
 
-
-// 2023.06.25 予約をキャンセルをするための確認画面を追加
-//要素を取得 let=変数 const=定数
+// https://zero-plus.io/media/javascript-modal/
+// https://recooord.org/modal-window-on-javascript/
+//要素を取得
 const modal = document.querySelector('.js-modal'),
     open = document.querySelector('.js-modal-open'),
     close = document.querySelector('.js-modal-close');
@@ -19,11 +19,15 @@ function modalOpen() {
 }
 open.addEventListener('click', modalOpen);
 
+
+
 //「閉じるボタン」をクリックしてモーダルを閉じる
 function modalClose() {
     modal.classList.remove('is-active');
 }
 close.addEventListener('click', modalClose);
+
+
 
 //「モーダルの外側」をクリックしてモーダルを閉じる
 function modalOut(e) {
@@ -32,7 +36,6 @@ function modalOut(e) {
     }
 }
 addEventListener('click', modalOut);
-
 
 
 
